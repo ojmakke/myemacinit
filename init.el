a7ccd5ac0d3383bac0a945a78c60a15109f5fc4d
O jM <ojmakke@yahoo.com>
2017-05-11 14:42:50 -0400

    CEDET version of completion

--- init.el
+++ init.el
@@ -3,78 +3,15 @@
 	     '("melpa" . "http://melpa.org/packages/")
 	     t)
 
+(global-ede-mode 1)
+(require 'semantic/db)
+(semantic-mode 1)
+(global-semantic-idle-completions-mode)
+(global-set-key (kbd "TAB") 'semantic-ia-complete-tip)
+(global-set-key [C-tab] 'semantic-ia-complete-symbol)
+(semantic-speedbar-analysis)
 
-;;(require 'company-irony-c-headers)
 
-;; =============
-;; irony-mode
-;; =============
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'c-mode-hook 'irony-mode)
-;; =============
-;; company mode
-;; =============
-(add-hook 'c++-mode-hook 'company-mode)
-(add-hook 'c-mode-hook 'company-mode)
-
-;; Replace the `completion-at-point' and `complete-symbol' bindings in
-;; irony-mode's buffers by irony-mode's function
-(defun my-irony-mode-hook ()
-(define-key irony-mode-map [remap completion-at-point]
-  'irony-completion-at-point-async)
-
-(define-key irony-mode-map [remap complete-symbol]
-  'irony-completion-at-point-async))
-
-(add-hook 'irony-mode-hook 'my-irony-mode-hook)
-
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-
-(eval-after-load 'company
-  '(add-to-list 'company-backends 'company-irony 'company-c-headers))
-   
-;; (optional) adds CC special commands to `company-begin-commands' in order to
-;; trigger completion at interesting places, such as after scope operator
-;;     std::|
-(add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
-;; =============
-;; flycheck-mode
-;; =============
-(add-hook 'c++-mode-hook 'flycheck-mode)
-(add-hook 'c-mode-hook 'flycheck-mode)
-(eval-after-load 'flycheck
-'(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
-;; =============
-;; eldoc-mode
-;; =============
-(add-hook 'irony-mode-hook 'irony-eldoc)
-;; ==========================================
-;; (optional) bind TAB for indent-or-complete
-;; ==========================================
-(defun irony--check-expansion ()
-(save-excursion
-  (if (looking-at "\\_>") t
-    (backward-char 1)
-    (if (looking-at "\\.") t
-      (backward-char 1)
-      (if (looking-at "->") t nil)))))
-(defun irony--indent-or-complete ()
-"Indent or Complete"
-(interactive)
-(cond ((and (not (use-region-p))
-            (irony--check-expansion))
-       (message "complete")
-       (company-complete-common))
-      (t
-       (message "indent")
-       (call-interactively 'c-indent-line-or-region))))
-(defun irony-mode-keys ()
-"Modify keymaps used by `irony-mode'."
-(local-set-key (kbd "TAB") 'irony--indent-or-complete)
-(local-set-key [tab] 'irony--indent-or-complete))
-(add-hook 'c-mode-common-hook 'irony-mode-keys)
-
-(setq irony-additional-clang-options '("-std=c++11"))
 
 (load-theme 'wombat t)
 
